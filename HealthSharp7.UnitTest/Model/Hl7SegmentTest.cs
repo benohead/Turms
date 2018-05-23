1fa69bf6feafdca16c7bd595d3679ac4abbbc970
Henri Benoit <henri.benoit@siemens-healthineers.com>
2018-05-23 23:39:12 +0200

    Revert "cleanup old files"
    This reverts commit 5c5e9142f0c9227f100b3c3952c736106871b11f.

---
+++ HealthSharp7.UnitTest/Model/Hl7SegmentTest.cs
@@ -0,0 +1,115 @@
+using System;
+using healthsharp7.Model;
+using HealthSharp7.Model;
+using Xunit;
+
+namespace healthsharp7.UnitTest.Model
+{
+    public class Hl7SegmentTest
+    {
+        private const string EvnSegmentTrimmed = "EVN|A01|20110613083617";
+        private const string EvnSegment = "EVN|A01|20110613083617|||";
+        private const string EvnSegmentDollarAsSeparatorTrimmed = "EVN$A01$20110613083617";
+        private const string EvnSegmentDollarAsSeparator = "EVN$A01$20110613083617$$$";
+
+        [Fact]
+        public void GettingNonExistingFieldShouldReturnFieldWithoutValue()
+        {
+            //Act
+            var segment = Hl7Segment.Parse(EvnSegmentTrimmed);
+
+            //Assert
+            Assert.Equal("", segment[4].ToString());
+        }
+
+        [Fact]
+        public void ParsingNullValueShouldThrowAnArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Hl7Segment.Parse(null));
+        }
+
+        [Fact]
+        public void SegmentNamesShouldBeThreeCharacterLong()
+        {
+            Assert.Throws<ArgumentException>(() => Hl7Segment.Parse("PI"));
+            Assert.Throws<ArgumentException>(() => Hl7Segment.Parse("PI|D"));
+            Assert.Throws<ArgumentException>(() => Hl7Segment.Parse("PIDD"));
+        }
+
+        [Fact]
+        public void SegmentOnlyWithNameShouldBeMarkedAsParsed()
+        {
+            //Act
+            var segment = Hl7Segment.Parse("EVN");
+            var segment2 = Hl7Segment.Parse("EVN|");
+
+            //Assert
+            Assert.True(segment.IsParsed);
+            Assert.True(segment2.IsParsed);
+        }
+
+        [Fact]
+        public void ShouldBeParsingMessageWithOtherSegmentSeparator()
+        {
+            //Act
+            var encoding = new Hl7Encoding {FieldSeparator = '$'};
+            var segment = Hl7Segment.Parse(EvnSegmentDollarAsSeparatorTrimmed, encoding);
+
+            //Assert
+            Assert.Equal(EvnSegmentDollarAsSeparatorTrimmed, segment.ToString());
+        }
+
+        [Fact]
+        public void ShouldLazyParseSegments()
+        {
+            //Act
+            var segment = Hl7Segment.Parse(EvnSegment);
+
+            //Assert
+            Assert.False(segment.IsParsed);
+        }
+
+        [Fact]
+        public void ShouldParseCreatedSegment()
+        {
+            //Arrange
+            var segment = new Hl7Segment() + "EVN" + "A01" + "20110613083617";
+
+            //Assert
+            Assert.Equal(EvnSegmentTrimmed, segment.ToString());
+        }
+
+        [Fact]
+        public void ShouldParseSegmentsOnlyWithName()
+        {
+            //Act
+            var segment = Hl7Segment.Parse("PID");
+
+            //Assert
+            Assert.Equal("PID", segment.Name);
+        }
+
+        [Fact]
+        public void ShouldReturnProperFieldValueUsingArrayNotation()
+        {
+            //Act
+            var segment = Hl7Segment.Parse(EvnSegment);
+
+            //Assert
+            Assert.Equal("A01", segment[1].ToString());
+            Assert.Equal("", segment[4].ToString());
+        }
+
+        [Fact]
+        public void ToStringReturnsOriginalStringWithoutTrailingPipes()
+        {
+            //Arrange
+            var segment = Hl7Segment.Parse(EvnSegment);
+            var segment2 = Hl7Segment.Parse(EvnSegmentDollarAsSeparator, new Hl7Encoding {FieldSeparator = '$'});
+
+            //Assert
+            Assert.Equal(EvnSegmentTrimmed, segment.ToString());
+            Assert.Equal(EvnSegmentDollarAsSeparatorTrimmed, segment2.ToString());
+        }
+    }
+}
---
+++ HealthSharp7/Model/Hl7Field.cs
@@ -0,0 +1,33 @@
+﻿using HealthSharp7.Model;
+
+namespace healthsharp7.Model
+{
+    public class Hl7Field : Hl7Element
+    {
+        public Hl7Field(string value) : this(value, new Hl7Encoding())
+        {
+        }
+
+        internal Hl7Field(string value, Hl7Encoding encoding)
+        {
+            Value = value;
+            Encoding = encoding;
+        }
+
+        public override string ToString()
+        {
+            EnsureFullyParsed();
+            return Value;
+        }
+
+        public static Hl7Field Parse(string field, Hl7Encoding encoding)
+        {
+            return new Hl7Field(field, encoding);
+        }
+
+        protected override void FullyParse()
+        {
+            IsParsed = true;
+        }
+    }
+}
